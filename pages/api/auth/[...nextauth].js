899fa57c0e3f55557dd635ece740e65f83ed2c7d
Rudis <82242977+rudisjcg@users.noreply.github.com>
2023-10-03 23:55:55 -0400

    Account Page - Features
    Account functions.
    
    Addresses Schema and Api endPoint

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,7 +3,7 @@ import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "@/lib/mongodb";
 
-const authOptions = {
+export const authOptions = {
   providers: [
     // OAuth authentication providers...
     GoogleProvider({
